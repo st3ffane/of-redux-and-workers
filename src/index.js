eb2fe95d894aafb4cac10834d11415ce87119ebd
PONTEINS Stéphane <stephane@do.ne>
2020-11-14 11:11:45 +0100

    Read Me doc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { middleware as workerMiddleware } from './middlewares/worker.middleware';
 export { middleware as workerAsPromiseMiddleware } from './middlewares/worker.as.promise';
 export { initWorker } from './worker';
+export { SUCCESS, ERROR, PREFIX } from '../middlewares/consts';
