500fbc19621a1a0d74f8107f30a6feddef89bb9b
PONTEINS Stéphane <stephane@do.ne>
2020-11-14 17:48:28 +0100

    bugfix bad path in export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { middleware as workerMiddleware } from './middlewares/worker.middleware';
 export { middleware as workerAsPromiseMiddleware } from './middlewares/worker.as.promise';
 export { initWorker } from './worker';
-export { SUCCESS, ERROR, PREFIX } from '../middlewares/consts';
+export { SUCCESS, ERROR, PREFIX } from './middlewares/consts';
